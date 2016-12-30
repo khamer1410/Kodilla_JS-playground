83af2d8996d6a2d31e97754876c2138a337e3584
Krzysztof Hamerszmit <kkhamer@gmail.com>
2016-12-30 20:36:09 +0100

    Add js task no1

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//Task: area of triangle
+
+var a = prompt('Podaj wartość a'),
+	h = prompt('Podaj wartość h'),
+	triangleArea = a*h/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
