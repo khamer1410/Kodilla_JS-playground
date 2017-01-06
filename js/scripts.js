32318d1104299d6644159d70acbc12dcd1655fe0
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-01-06 19:50:43 +0100

    Improve script

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 function getTriangleArea(a, h) {
 	if (a > 0 && h > 0) {
-		return a*h/2;
+		return a * h / 2;
 	} else {
-		console.log('Nieprawidłowe dane');
+		return ('Nieprawidłowe dane');
 	}
 };
 
 console.log(getTriangleArea(10,6));
 
-var triangle1 = getTriangleArea(5,4)
-var triangle2 = getTriangleArea(13,6)
+var triangle1 = getTriangleArea(5,4);
+var triangle2 = getTriangleArea(13,6);
 var triangle3 = getTriangleArea(5,20);
 
 console.log('Nasze pola to: ' + triangle1 + ','+ triangle2 + ',' + triangle3);
