64074132b9fcf1d6d4fb3c1a5a05442a4cd55ef4
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-01-06 20:49:58 +0100

    Add loop ex - Christmas tree v1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+function printTree(rows) {
+  var star;
+	for (var i = 1; i < rows; i++) {
+	  star = "";
+		for (var j = 0; j < i * 2 -1; j++) {
+			star += "*";
+		}
+		console.log(star);
+	}
+}
+
+var treeHeight = prompt('Jak wysoka ma być choinka?');
+console.log(printTree(treeHeight));
+//jak zrobić, żeby choinka pojawiła się jako alert? Da sie zmienną przekształcić w string i wyświetlić?
