000d37eb1e646b8298127e560fe3b58c4e0d9fae
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-02-01 21:50:08 +0100

    center tree position + trunk

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
 
-function printTree(rows) {
-	var star;
-	for (var i = 1; i < rows; i++) {
-		star = "";
-		for (var j = 0; j < i * 2 -1; j++) {
-			star += "*";
-		}
-		console.log(star);
-	}
-}
-
 var treeHeight = prompt('Jak wysoka ma być choinka?');
 console.log(printTree(treeHeight));
+
+function printTree(n) {
+
+	for (var i = 1; i <= n; i++) { 
+        var star ='' ; 
+        for (var j=0; j < i*2-1; j++) { 
+                star += '*'; 
+                var row = ''; 
+                for (var k = 0; n - i > k; k++) { 
+                        row+= ' '; 
+                } 
+        } 
+        var treeRow = row + star + row; 
+        console.log(treeRow); 
+        
+        if (star.length < 2) { 
+                var treeTrunk = treeRow; 
+        } 
+	} 
+	var trunkHeight = prompt('podaj długość pnia'); 
+	var x = 0; 
+	while (x < trunkHeight) { 
+        console.log(treeTrunk); 
+        x++; 
+	} 
+}
+
 //jak zrobić, żeby choinka pojawiła się jako alert? Da sie zmienną przekształcić w string i wyświetlić?
